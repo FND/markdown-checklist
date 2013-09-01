8b4ed5c3d50713bbc46aee4d59c852bdd47c2fd1
Jonathan Eunice <jonathan.eunice@gmail.com>
2013-09-01 15:31:37 -0400

    Renamed markdown-checklist to mdx_checklist so it can be easily auto-found and imported.  Added tox.ini, pytest.ini, and .gitignore. Tested all modifications on py26, py27, py32, py33, and pypy.

---
+++ mdx_checklist/__init__.py
@@ -0,0 +1,7 @@
+from mdx_checklist.meta import *
+from mdx_checklist.extension import ChecklistExtension, makeExtension
+
+__version__ = VERSION
+__license__ = LICENSE
+__author__ = AUTHOR
+__doc__ = DESC
