7776397485c0456cbd4374d961b3a8ed07affafe
Tobias Hochgürtel <tobias.hochguertel@hochguertel.biz>
2015-07-15 15:14:21 +0200

    OmniMarkupPreviewer Markdown-Extension: checklists.

--- markdown_checklist/extension.py
+++ markdown_checklist/extension.py
@@ -25,14 +25,26 @@ class ChecklistPostprocessor(Postprocessor):
     """
 
     pattern = re.compile(r'<li>\[([ Xx])\]')
+    item_pattern = re.compile(r'(<li.*<input type="checkbox".*>)(.*)(</li>)')
 
     def run(self, html):
         html = re.sub(self.pattern, self._convert_checkbox, html)
-        before = '<ul>\n<li><input type="checkbox"'
+        before = '<ul>\n<li class="task-list-item"><input type="checkbox"'
         after = before.replace('<ul>', '<ul class="checklist">')
-        return html.replace(before, after)
+        checked_html = html.replace(before, after)
+        checked_labeld_html = re.sub(self.item_pattern, self._convert_label, checked_html)
+        return checked_labeld_html
 
     def _convert_checkbox(self, match):
         state = match.group(1)
+        activeState = ''
         checked = ' checked' if state != ' ' else ''
-        return '<li><input type="checkbox" disabled%s>' % checked
+        activeAttr = ' disabled' if activeState != ' ' else ''
+        return '<li class="task-list-item"><input type="checkbox" %s>' % checked
+
+    def _convert_label(self, match):
+        before_item = match.group(1)
+        item = match.group(2)
+        after_item = match.group(3)
+        return '%s<label>%s</label>%s' % (before_item, item, after_item)
+
